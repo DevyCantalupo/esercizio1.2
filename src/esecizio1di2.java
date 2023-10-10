484ed1adc1f0aa3d20917d0e4d3622e2f5182a92
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-10 23:04:39 +0200

    First Commit

---
+++ src/esecizio1di2.java
@@ -0,0 +1,9 @@
+public class esecizio1di2 {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+
+        String c = "" + a + b;
+        System.out.println("c = " + c);
+    }
+}
